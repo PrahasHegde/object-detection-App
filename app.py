9669474a30d0fba120bfe5baa30f39e8d14fe467
prahas10 <108119173+PrahasHegde@users.noreply.github.com>
2024-03-10 22:54:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, Response
+import cv2
+import math
+from ultralytics import YOLO
+
+app = Flask(__name__)
+
+# Load the YOLO model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# Object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while True:
+        success, img = cap.read()
+
+        if not success:
+            break
+
+        # Resize the frame to a larger size
+        img = cv2.resize(img, (1150, 650))  # Adjust the size as needed
+
+        results = model(img, stream=True)
+
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+                confidence = math.ceil((box.conf[0] * 100)) / 100
+                class_name = classNames[int(box.cls[0])]
+
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (0, 255, 0)
+                thickness = 2
+
+                cv2.putText(img, class_name, org, font, fontScale, color, thickness)
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
